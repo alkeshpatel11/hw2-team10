c87157b3f9ec6aadb10da220421e64545871f564
alkesh <alkeshku@andrew.cmu.edu>
2012-12-06 01:07:43 -0500

    More yamls added

--- hw2-team10/src/main/java/edu/cmu/lti/oaqa/openqa/test/team10/keyterm/SimilarNGramExtractor.java
+++ hw2-team10/src/main/java/edu/cmu/lti/oaqa/openqa/test/team10/keyterm/SimilarNGramExtractor.java
@@ -1,43 +1,91 @@
 package edu.cmu.lti.oaqa.openqa.test.team10.keyterm;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import edu.cmu.lti.bio.alkesh.customtypes.GeneCount;
 import edu.cmu.lti.bio.alkesh.genetrainer.NGramLuceneWrapper;
+import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
 
-public class SimilarNGramExtractor {
+/**
+ * 
+ * @author alkesh
+ * 
+ */
+public class SimilarNGramExtractor extends AbstractKeytermExtractor {
+
+	int nSimilarGrams = 10;
+	double filterThreshold = 0.30;
+	NGramLuceneWrapper nGramLuceneWrapper;
+
+	@Override
+	public void initialize(UimaContext aContext)
+			throws ResourceInitializationException {
+		super.initialize(aContext);
+		nSimilarGrams = (Integer) aContext.getConfigParameterValue(
+				"ngram-size", "10");
+		filterThreshold = (Double) aContext.getConfigParameterValue(
+				"ngram-score-threshold", "0.30");
+
+		nGramLuceneWrapper = new NGramLuceneWrapper();
+	}
 
 	public ArrayList<GeneCount> getSynonyms(String term) throws Exception {
 
-		NGramLuceneWrapper main = new NGramLuceneWrapper();
 		// main.createIndex();
-		ArrayList<GeneCount> geneList = main.searchIndex(term, 100);
+
+		ArrayList<GeneCount> geneList = nGramLuceneWrapper.searchIndex(term,
+				nSimilarGrams);
 		ArrayList<GeneCount> selectedList = new ArrayList<GeneCount>();
 		if (geneList.size() > 0) {
-			
-			double thr=geneList.get(0).getCount()*0.30;
+
+			double thr = geneList.get(0).getCount() * filterThreshold;
 			for (int i = 0; i < geneList.size(); i++) {
 				// System.out.println(geneList.get(i).getGeneName()+"\t"+geneList.get(i).getCount());
 				if (geneList.get(i).getCount() > thr) {
 					selectedList.add(geneList.get(i));
-				}
-				if (i > 10) {
+				} else {
 					break;
 				}
 			}
 		}
 		return selectedList;
 	}
-	
-	public static void main(String args[]){
-		try{
-			SimilarNGramExtractor extractor=new SimilarNGramExtractor();
-			ArrayList<GeneCount>list=extractor.getSynonyms("beta-amyloid precursor");
-			for(int i=0;i<list.size();i++){
+
+	@Override
+	protected List<Keyterm> getKeyterms(String term) {
+		List<Keyterm> keyterms = new ArrayList<Keyterm>();
+		ArrayList<GeneCount> similarNgrams = new ArrayList<GeneCount>();
+		try {
+			getSynonyms(term);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return keyterms;
+		}
+
+		for (int i = 0; i < similarNgrams.size(); i++) {
+			keyterms.add(new Keyterm(similarNgrams.get(i).getGeneName()));
+		}
+
+		return keyterms;
+
+	}
+
+	public static void main(String args[]) {
+		try {
+			SimilarNGramExtractor extractor = new SimilarNGramExtractor();
+			ArrayList<GeneCount> list = extractor
+					.getSynonyms("beta-amyloid precursor");
+			for (int i = 0; i < list.size(); i++) {
 				System.out.println(list.get(i).getGeneName());
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
