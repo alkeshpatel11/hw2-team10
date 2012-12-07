a76a7745776b7aa5dff6a97276983117a7c60a89
alkesh <alkeshku@andrew.cmu.edu>
2012-12-07 16:11:54 -0500

    path from config

--- hw2-team10/src/main/java/edu/cmu/lti/oaqa/openqa/test/team10/keyterm/EnhancedGOSynonymExtractor.java
+++ hw2-team10/src/main/java/edu/cmu/lti/oaqa/openqa/test/team10/keyterm/EnhancedGOSynonymExtractor.java
@@ -18,114 +18,103 @@ import edu.cmu.lti.oaqa.framework.data.Keyterm;
 
 public class EnhancedGOSynonymExtractor extends AbstractKeytermExtractor {
 
-  private int MAX_SIZE;
-  private String GoFilePath;
-  public NGramLuceneWrapper nGramLuceneWrapper;
-  @Override
-  public void initialize(UimaContext c) throws ResourceInitializationException {
-   
-    super.initialize(c);
-    MAX_SIZE = (Integer)c.getConfigParameterValue("max_size");
-    GoFilePath = (String) c.getConfigParameterValue("goFilePath","data/gene_ontology_ext.obo");
-    nGramLuceneWrapper=new NGramLuceneWrapper();
-  }
-  
-  
-  @Override
-  protected List<Keyterm> getKeyterms(String question) {
-    question = question.replace('?', ' ');
-    question = question.replace('(', ' ');
-    question = question.replace('[', ' ');
-    question = question.replace(')', ' ');
-    question = question.replace(']', ' ');
-    question = question.replace('/', ' ');
-    question = question.replace('\'', ' ');
-    
-    String[] questionTokens = question.split("\\s+");
-    List<Keyterm> keyterms = new ArrayList<Keyterm>();  
-    ArrayList<GeneCount> ngramReturn = new ArrayList<GeneCount>();
-    ArrayList<GeneCount> ngramTotal = new  ArrayList<GeneCount>();
-    ArrayList<String> ngramSynonyms = new ArrayList<String>();
-    //Simply adding question terms to the list of keyterms
-    for(int i=0; i<questionTokens.length;i++)
-    {
-        keyterms.add(new Keyterm(questionTokens[i]));
-    }
+	private int MAX_SIZE;
+	private String GoFilePath;
+	public NGramLuceneWrapper nGramLuceneWrapper;
 
-    
-  //Getting synonyms from NGram
-    
-    SimilarNGramExtractor ngramExtractor = new SimilarNGramExtractor();
-    for(String ngramSyn:questionTokens)
-    {
-    try {
-      ngramReturn= ngramExtractor.getSynonyms(ngramSyn);
-      
-      for(GeneCount gc: ngramReturn)
-      {
-          ngramTotal.add(gc);   
-      }
-      
-    } catch (Exception e) {
-      
-      e.printStackTrace();
-    }
-    }
-    
-    
-    //Calling GO synonym extractor
-    int j=0;
-    for(GeneCount gca: ngramTotal)
-    {
-      ngramSynonyms.add(gca.getGeneName());
-      
-    }
-    System.out.println("NGRAM =" + ngramSynonyms.toString());
-    ArrayList<String> SynFromGO = findBestRelatedGenesFromGO(ngramSynonyms);
-    
-    
-    if(SynFromGO.size()>0)
-    {
-      for(int i=0; i< MAX_SIZE;i++)
-      {
-         keyterms.add(new Keyterm(SynFromGO.get(i)));
-         System.out.println(SynFromGO.get(i));
-      }
-    }
-    
-    
-    return keyterms;    
-  }
+	@Override
+	public void initialize(UimaContext c)
+			throws ResourceInitializationException {
 
-  private ArrayList<String> findBestRelatedGenesFromGO(ArrayList<String> genes)
-  {  
-  ArrayList<String> geneListToSend = new ArrayList<String>();
-  try {
+		super.initialize(c);
+
+		MAX_SIZE = (Integer) c.getConfigParameterValue("max_size");
+		GoFilePath = (String) c.getConfigParameterValue("goFilePath");
+
+		nGramLuceneWrapper = new NGramLuceneWrapper();
+	}
+
+	@Override
+	protected List<Keyterm> getKeyterms(String question) {
+		question = question.replace('?', ' ');
+		question = question.replace('(', ' ');
+		question = question.replace('[', ' ');
+		question = question.replace(')', ' ');
+		question = question.replace(']', ' ');
+		question = question.replace('/', ' ');
+		question = question.replace('\'', ' ');
+
+		String[] questionTokens = question.split("\\s+");
+		List<Keyterm> keyterms = new ArrayList<Keyterm>();
+		ArrayList<GeneCount> ngramReturn = new ArrayList<GeneCount>();
+		ArrayList<GeneCount> ngramTotal = new ArrayList<GeneCount>();
+		ArrayList<String> ngramSynonyms = new ArrayList<String>();
+		// Simply adding question terms to the list of keyterms
+		for (int i = 0; i < questionTokens.length; i++) {
+			keyterms.add(new Keyterm(questionTokens[i]));
+		}
+
+		// Getting synonyms from NGram
+
+		SimilarNGramExtractor ngramExtractor = new SimilarNGramExtractor();
+		for (String ngramSyn : questionTokens) {
+			try {
+				ngramReturn = ngramExtractor.getSynonyms(ngramSyn);
+
+				for (GeneCount gc : ngramReturn) {
+					ngramTotal.add(gc);
+				}
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		// Calling GO synonym extractor
+		int j = 0;
+		for (GeneCount gca : ngramTotal) {
+			ngramSynonyms.add(gca.getGeneName());
+
+		}
+		System.out.println("NGRAM =" + ngramSynonyms.toString());
+		ArrayList<String> SynFromGO = findBestRelatedGenesFromGO(ngramSynonyms);
+
+		if (SynFromGO.size() > 0) {
+			for (int i = 0; i < MAX_SIZE; i++) {
+				keyterms.add(new Keyterm(SynFromGO.get(i)));
+				System.out.println(SynFromGO.get(i));
+			}
+		}
+
+		return keyterms;
+	}
+
+	private ArrayList<String> findBestRelatedGenesFromGO(ArrayList<String> genes) {
+
+		ArrayList<String> geneListToSend = new ArrayList<String>();
+		try {
 			OBOGraph graph = new OBOGraph(new FileReader(new File(GoFilePath)));
-        
-    for(int i=0; i<genes.size(); i++)
-    {
-      
-      ArrayList<OBONode> oboNodes = graph.search(genes.get(i));
-      for(OBONode o: oboNodes)
-      {
-               
-        for(String syn: o.getAllSynonyms())
-        {          
-          geneListToSend.add(syn);
-          
-        }
-      }
-    }
-  } catch (FileNotFoundException e) {
-    
-    e.printStackTrace();
-  }
-  
-  return geneListToSend;
-  }
-  
-  public static void main(String args[]) {
+			for (int i = 0; i < genes.size(); i++) {
+
+				ArrayList<OBONode> oboNodes = graph.search(genes.get(i));
+				for (OBONode o : oboNodes) {
+
+					for (String syn : o.getAllSynonyms()) {
+						geneListToSend.add(syn);
+
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+		return geneListToSend;
+	}
+
+	public static void main(String args[]) {
 		try {
 			EnhancedGOSynonymExtractor extractor = new EnhancedGOSynonymExtractor();
 			List<Keyterm> list = extractor
@@ -138,6 +127,4 @@ public class EnhancedGOSynonymExtractor extends AbstractKeytermExtractor {
 		}
 	}
 
-  
-  
 }
